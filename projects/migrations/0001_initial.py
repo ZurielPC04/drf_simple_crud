d22a294f5ba3ba1c43dd695477be7c476ff77e44
Zuriel Palafox <zurielpc04@gmail.com>
2026-01-11 19:25:55 -0600

    Antes de empezar a aescalar

--- projects/migrations/0001_initial.py
+++ projects/migrations/0001_initial.py
@@ -17,11 +17,8 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=100)),
                 ('description', models.TextField()),
-                ('description 2', models.TextField()),
-                ('description 3', models.TextField()),
                 ('technology', models.CharField(max_length=50)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
-
             ],
         ),
     ]
